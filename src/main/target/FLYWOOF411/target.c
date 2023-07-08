c112ff8bc1b29da5515ae875a6a3a9cead6baf4d
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2023-07-08 13:41:32 +0200

    Revert "added gps jammer controll to beeper"

--- src/main/target/FLYWOOF411/target.c
+++ src/main/target/FLYWOOF411/target.c
@@ -71,5 +71,4 @@ const int timerHardwareCount = sizeof(timerHardware) / sizeof(timerHardware[0]);
 void targetConfiguration(void)
 {
     pinioBoxConfigMutable()->permanentId[0] = BOX_PERMANENT_ID_USER1;
-    pinioBoxConfigMutable()->permanentId[1] = BOX_PERMANENT_ID_USER2;
 }
