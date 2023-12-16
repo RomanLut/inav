221c74942b90ecd40fe2d2bd6f6c0e1c5c087b74
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2023-12-16 14:25:38 +0100

    allow arming override to disable dshot beeper

--- src/main/io/beeper.c
+++ src/main/io/beeper.c
@@ -345,7 +345,7 @@ void beeperUpdate(timeUs_t currentTimeUs)
     if (!beeperIsOn) {
 #ifdef USE_DSHOT
         if (isMotorProtocolDshot() && !areMotorsRunning() && beeperConfig()->dshot_beeper_enabled
-            && currentTimeUs - lastDshotBeeperCommandTimeUs > getDShotBeaconGuardDelayUs())
+            && currentTimeUs - lastDshotBeeperCommandTimeUs > getDShotBeaconGuardDelayUs() && !checkStickPosition(YAW_HI))
         {
             lastDshotBeeperCommandTimeUs = currentTimeUs;
             sendDShotCommand(beeperConfig()->dshot_beeper_tone);
