6a273ba2843c77899447d9b45dfc5398deb08476
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2026-08-08 15:32:39 +0200

    addedbach file tobuild firmare

---
+++ run-docker-compiler-FLYWOOF411_DFSpirit2s.bat
@@ -0,0 +1,5 @@
+@rem -rm = automatically clean up the container and remove the file system when the container exits
+@rem -it = starts the container in the interactive mode that allows you to interact with /bin/bash of the container
+@rem -v = link local folder to folder inside machine
+docker run --rm -it -u root -v //d/Github/inav/inav:/src inav-build FLYWOOF411_DFSpirit2s
+
